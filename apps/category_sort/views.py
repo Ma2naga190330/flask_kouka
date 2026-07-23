303b5f7d37a36d2011aac09333f03ea073099071
松永悠志 <myz0330fuklow0330@gmail.com>
2026-07-23 12:32:08 +0900

    fix add comment

--- apps/category_sort/views.py
+++ apps/category_sort/views.py
@@ -3,7 +3,7 @@ import torch
 from sentence_transformers import SentenceTransformer
 from pathlib import Path
 from apps.category_sort.forms import CategoryForm
-
+from flask_login import login_required
 cs = Blueprint(
     "categories",
     __name__,
@@ -11,40 +11,33 @@ cs = Blueprint(
     static_folder="static"
 )
 
+@login_required
 @cs.route("/", methods=["GET","POST"])
 def index():
     form = CategoryForm()
     if form.validate_on_submit():
         text = form.category_name.data
         category = exe_categories(text)
-        print("------------------------------------")
         return render_template("category_sort/categories.html",text=text,category=category)
     return render_template("category_sort/index.html",form=form)
 
 
 def exe_categories(text):
-    model_path = Path(current_app.root_path, "model.pt")
-    
-    # 1. ベースモデルをロード
-    model = SentenceTransformer("tohoku-nlp/bert-base-japanese-v3") # ※学習時に使用したベースモデル名
-    
-    # 2. torch.load で重みを読み込む
-    state_dict = torch.load(model_path, map_location="cpu")
-    
-    # 3. SentenceTransformer 全体に重みをロード（strict=False で微細なキー違いを許容）
-    model.load_state_dict(state_dict, strict=False)
-    # model =  SentenceTransformer(str(Path(current_app.root_path,"model.pt")))
+    # model = torch.load(Path(current_app.root_path, "model.pt"))
+    # モデルをロード
+    model = SentenceTransformer("tohoku-nlp/bert-base-japanese-v3")
+    # 分類カテゴリーの取得
     category = current_app.config["CATEGORIES"]
+    # text,categoryを推論用にエンコード
     en_text=model.encode(text)
     en_category = model.encode(category)
-
+    # 推論の実行
     similarities = model.similarity(en_text, en_category)
     print(similarities)
-    # tensor([[0.7245, 0.1032, 0.0511]]) の中で最大値の場所（この場合 0 番目）を取得
+    # 分類したカテゴリーの中で最大値のindexを取得
     best_idx = similarities[0].argmax().item()
 
     # カテゴリーリストから名称を取得
     predicted_category = category[best_idx]
 
-    print(f"分類結果: {predicted_category}")
     return predicted_category
